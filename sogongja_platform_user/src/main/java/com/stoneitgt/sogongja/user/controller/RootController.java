34c81c1f6f2c1927d8359a4001a1e61672a65397
audxor34 <audxor34@gmail.com>
2022-09-06 17:48:07 +0900

    * 카카오 회원가입 개발중...

--- sogongja_platform_user/src/main/java/com/stoneitgt/sogongja/user/controller/RootController.java
+++ sogongja_platform_user/src/main/java/com/stoneitgt/sogongja/user/controller/RootController.java
@@ -3,8 +3,11 @@ package com.stoneitgt.sogongja.user.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.stoneitgt.sogongja.user.security.SocialLoginSupport;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.stoneitgt.sogongja.domain.LoginForm;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
+@RequiredArgsConstructor
 public class RootController extends BaseController {
+
+	private final SocialLoginSupport socialLoginSupport;
 
 	@GetMapping(value = { "/", "/index" })
 	public String index(Model model) {
@@ -26,11 +34,12 @@ public class RootController extends BaseController {
 	}
 
 	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
-	public String login(@ModelAttribute LoginForm loginForm, Model model) {
+	public String login(@ModelAttribute LoginForm loginForm, Model model, HttpServletRequest request) {
 		// 로그인 실패시 메인으로 리타이렉트 됨 따라서 리다이레트 주석처리
 		/*
 		 * if (authenticationFacade.isAuthenticated()) { return "redirect:/"; }
 		 */
+		socialLoginSupport.setSocialOauthUrl(request, model);
 		model.addAttribute("loginForm", loginForm);
 		return "pages/login";
 	}
