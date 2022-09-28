a405f66e7de4fcee8b56916420a91bf2b6aa79b0
mlhj6010 <mlhj6010@gmail.com>
2022-09-28 14:37:29 +0900

    [pub] ① 배너추가 페이지 퍼블리싱 수정(썸네일 노출되게 설정)

--- sogongja_platform_admin/src/main/resources/static/assets/js/file-upload.js
+++ sogongja_platform_admin/src/main/resources/static/assets/js/file-upload.js
@@ -1,5 +1,6 @@
 (function() {
     'use strict'
+
     $(document).ready(function() {
         $(document).on('click', '.file-upload-browse', function(e) {
             var file = $(this).closest('div.attach-file').find('input.file-upload-default');
@@ -9,5 +10,33 @@
         $(document).on('change', '.file-upload-default', function(e) {
             $(this).parent().find('input.file-upload-title').val($(this).val().replace(/C:\\fakepath\\/i, ''));
         });
+
     });
 })();
+
+// PC
+function readURLPC(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('previewImgPc').src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById('previewImgPc').src = "https://dummyimage.com/500x500/ffffff/000000.png&text=preview+image";
+    }
+}
+
+
+// 모바일
+function readURLM(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('previewImgM').src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById('previewImgM').src = "https://dummyimage.com/500x500/ffffff/000000.png&text=preview+image";
+    }
+}
