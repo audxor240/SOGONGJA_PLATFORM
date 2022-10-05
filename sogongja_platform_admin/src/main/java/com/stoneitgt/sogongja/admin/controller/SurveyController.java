9296f0d68726a43ee171fa9eba0b499d64822930
audxor34 <audxor34@gmail.com>
2022-10-05 11:55:27 +0900

    * 설문관리 등록 페이지 연결

--- sogongja_platform_admin/src/main/java/com/stoneitgt/sogongja/admin/controller/SurveyController.java
+++ sogongja_platform_admin/src/main/java/com/stoneitgt/sogongja/admin/controller/SurveyController.java
@@ -1,14 +1,17 @@
 package com.stoneitgt.sogongja.admin.controller;
 
+import com.stoneitgt.common.GlobalConstant;
 import com.stoneitgt.common.Paging;
 import com.stoneitgt.sogongja.admin.domain.EducationParameter;
 import com.stoneitgt.sogongja.admin.service.EducationService;
+import com.stoneitgt.sogongja.domain.Education;
 import com.stoneitgt.util.StoneUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
@@ -49,6 +52,30 @@ public class SurveyController extends BaseController {
         model.addAttribute("supportOrg", getCodeList("SUPPORT_ORG", "전체"));
 
         return "pages/survey/survey_list";
+    }
+
+    @GetMapping("/{surveySeq}")
+    public String serveyView(@PathVariable int surveySeq, @ModelAttribute EducationParameter params, Model model) {
+
+        Education education = educationService.getEducation(surveySeq);
+
+        model.addAttribute("education", education);
+        model.addAttribute("menuCode", params.getMenuCode());
+        //model.addAttribute("breadcrumb", getBreadcrumb(params.getMenuCode()));
+        HashMap<String, Object> breadcrumb = new HashMap<String, Object>();
+        breadcrumb.put("parent_menu_name", "시스템 관리");
+        breadcrumb.put("menu_name", "설문 관리");
+        model.addAttribute("breadcrumb", breadcrumb);
+
+        model.addAttribute("pageParams", getBaseParameterString(params));
+        model.addAttribute("fileList", getFileList(GlobalConstant.FILE_REF_TYPE.EDUCATION, surveySeq));
+        model.addAttribute("imageList", getFileList(GlobalConstant.FILE_REF_TYPE.EDUCATION_IMAGE, surveySeq));
+        model.addAttribute("category1", getCodeList("CATEGORY_1"));
+        model.addAttribute("category2", getCodeRefList("CATEGORY_2", education.getCategory1(), ""));
+        model.addAttribute("category3", getCodeRefList("CATEGORY_3", education.getCategory2(), ""));
+        model.addAttribute("supportOrg", getCodeList("SUPPORT_ORG"));
+
+        return "pages/survey/survey_form";
     }
 
 }
