c2f71d57b965c61a0226c931f5110daeae34fb7a
mlhj6010 <mlhj6010@gmail.com>
2022-10-24 14:36:16 +0900

    [pub] ① 사용자  > 공지사항 및 게시판 sass 수정 ② 관리자  > 이용자 유형 js수정

--- sogongja_platform_admin/src/main/resources/static/assets/js/pages/setting/user_form.js
+++ sogongja_platform_admin/src/main/resources/static/assets/js/pages/setting/user_form.js
@@ -8,5 +8,38 @@
         $('.pop_pw_close').on('click', function() {
             $('.pop_pw_wrap').removeClass('on');
         });
+
+        $('#useCategory01').change(function(){
+            $("input:radio[name='useCategory']").prop("chcecked",false);
+            $("#sub01").show();
+            $("#sub02").hide();
+            $("#sub03").hide();
+            $("#useCategory01_01").prop("checked",true);
+        });
+
+        $('#useCategory02').change(function(){
+            $("input:radio[name='useCategory']").prop("chcecked",false);
+            $("#sub01").hide();
+            $("#sub02").show();
+            $("#useCategory02_01").prop("checked",true);
+            $("#sub03").hide();
+        });
+
+        $('#useCategory03').change(function(){
+            $("input:radio[name='useCategory']").prop("chcecked",false);
+            $("#sub01").hide();
+            $("#sub02").hide();
+            $("#sub03").show();
+            $("#useCategory03_01").prop("checked",true);
+        });
+
+        $('#useCategory04').change(function(){
+            $("#sub01").hide();
+            $("#sub02").hide();
+            $("#sub03").hide();
+        });
+
+
+
     });
 })();
