eefd9cf23b5ad9592022df7aebe302919e251434
audxor34 <audxor34@gmail.com>
2022-11-02 15:06:58 +0900

    * 사용자   1. 맞춤 컨설팅     - 추천 컨설팅, 다른사용자가 자주 찾는 컨설팅 리스트 출력   	- 찜하기, 수강하기 기능 추가   	- 이미 받은 컨설팅 감추기 기능 추가

--- sogongja_platform_user/src/main/resources/static/assets/js/pages/solution/education_list.js
+++ sogongja_platform_user/src/main/resources/static/assets/js/pages/solution/education_list.js
@@ -58,11 +58,14 @@
         }
         form.submit();
     });
+
+
 })();
 
 function favorite(seq){
     let data = {
-        seq: seq
+        seq: seq,
+        type: "edu"
     };
 
     var token = $("meta[name='_csrf']").attr("content");
