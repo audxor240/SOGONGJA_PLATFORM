3fadee25722cd61da8bdb1dd8046200763265c58
audxor34 <audxor34@gmail.com>
2022-11-08 13:00:58 +0900

    * 관리자   1. 지원 및 정책관리     - 등록시 url정보 추가     - 전체 체크선택 기능 수정 * 사용자   1. 지원 및 정책관리     - 검색 기능 수정     - 리스트 출력   2. 커머스 연구소     - 하위 페이지에서 '소공자 물어방' header정보 나오지 않는 오류 수정

--- sogongja_platform_admin/src/main/resources/static/assets/js/pages/board/project_list.js
+++ sogongja_platform_admin/src/main/resources/static/assets/js/pages/board/project_list.js
@@ -27,30 +27,3 @@
         });
     });
 })();
-
-
-function validationForm(){
-
-
-    var json_sub = new Object();
-    var projectList = new Array();
-
-    if($('input:checkbox[name="project_chk"]:checked').length == 0){
-        alert("선택 된 것이 없습니다.");
-        return false;
-    }
-
-    $('input:checkbox[name="project_chk"]:checked').each(function(item,index) {
-        var json = new Object();
-        json.type = $(this).parent().parent().find("#type").text();     //분류
-        json.title = $(this).parent().parent().find("#title").text();   //제목
-        json.year = $(this).parent().parent().find("#year").text();     //년도
-        json.areaNm = $(this).parent().parent().find("#areaNm").text(); //지역
-        projectList.push(json);
-    });
-    var data = {
-        "projectList":projectList
-    };
-    $("[name=projectList]").val(JSON.stringify(data));
-    return true;
-}
