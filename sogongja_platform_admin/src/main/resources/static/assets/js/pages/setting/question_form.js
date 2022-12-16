4afb7b576f4498649dc9dbd3871f2e189a7b1160
audxor34 <audxor34@gmail.com>
2022-12-16 11:29:18 +0900

    * 관리자   1. 질문관리     - 상세페이지에서 답변을 추가하고 수정할 경우 404에러 발생하는 오류 수정(답변에 콤마(,)가 포함되어 있을 경우 에러 발생함)   2. 커머스 연구소     - 인구수(,) 및 퍼센트율(%) 표시 수정
    * 사용자
      1. 커머스 연구소 > 상점/지역 연구
        - 해당 지역의 커뮤니티 글 불러올때 415 에러로 간헐적 ajax호출이 안되는 오류 수정
        - ajax 호출시 contentType을 Json으로 고정

--- sogongja_platform_admin/src/main/resources/static/assets/js/pages/setting/question_form.js
+++ sogongja_platform_admin/src/main/resources/static/assets/js/pages/setting/question_form.js
@@ -281,15 +281,16 @@ function validationForm(){
         }
 
         var cnt = 0;
+        var answerTitleStr = "";
         $('[name=answerArr]').each(function(){
             var tag = "";
             //answerTitleList.push($(this).children("#answer_"+cnt).find('p').text());
             answerTitleList.push($(this).children(".item_list").find('p').text());
+            answerTitleStr += $(this).children(".item_list").find('p').text()+"|";
 
             //$("[name=matching_"+cnt+"]").find('input').each(function(){
             $(this).find(".item_tag_wrap").find(".item_tag").find('input').each(function(){
                 tag += $(this).val()+"|";
-                console.log("tag >>> "+tag );
 
             });
             tag = tag.slice(0,-1);
@@ -298,10 +299,12 @@ function validationForm(){
             cnt++;
         });
 
+        answerTitleStr = answerTitleStr.slice(0,-1);
+
         $("[name=answerTitleList]").val(answerTitleList);
         $("[name=answerTagList]").val(answerTagList);
-        console.log("answerTitleList :: "+answerTitleList);
-        console.log("answerTagList :: "+answerTagList);
+        $("[name=answerTitleStr]").val(answerTitleStr);
+
     }else{
         var answerType = $("#answerType").val();
         if(questionType == "add" && answerType == "2"){ //답변항목[주소]
