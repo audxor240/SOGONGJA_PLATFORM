b4ae78fe27a65c9a8f4e7b7ecbf1a1ed90538643
yeonhuha <soaoz2308@naver.com>
2022-12-23 12:39:15 +0900

    소공자 솔루션 커머스 도시공간분석 토지 특성 기본 설정

---
+++ sogongja_platform_user/src/main/java/com/stoneitgt/sogongja/user/service/CommerceService.java
@@ -0,0 +1,35 @@
+package com.stoneitgt.sogongja.user.service;
+
+import com.stoneitgt.sogongja.user.mapper.UrbanMapper;
+import com.stoneitgt.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class CommerceService {
+
+    @Autowired
+    private UrbanMapper urbanMapper;
+
+    public List<Map<String, Object>> getGrid(Map<String, Object> params) {
+        List<Map<String, Object>> list = urbanMapper.getGrid(params);
+
+        List<Map<String, Object>> locationList = urbanMapper.getGridLocation();
+
+        for (Map<String, Object> map : list) {
+            String gridCd = map.get("grid_cd").toString();
+
+            List<Map<String, Object>> path = (List<Map<String, Object>>) locationList.stream()
+                    .filter(m -> m.get("grid_cd").toString().equals(gridCd)).collect(Collectors.toList());
+
+            map.put("path", path);
+
+        }
+        return list;
+    }
+
+}
