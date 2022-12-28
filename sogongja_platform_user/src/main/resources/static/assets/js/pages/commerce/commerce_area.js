68d69857247617082a8cb6fb8c732ad555efc6d5
juneyoung <jadw951623@gmail.com>
2022-12-28 17:25:55 +0900

    feat. kakao map contorl

--- sogongja_platform_user/src/main/resources/static/assets/js/pages/commerce/commerce_area.js
+++ sogongja_platform_user/src/main/resources/static/assets/js/pages/commerce/commerce_area.js
@@ -47,12 +47,12 @@ kakao.maps.event.addListener(map, 'mousemove', function (mouseEvent) {
         var circle = new kakao.maps.Circle({
             center: mouseEvent.latLng,  // 원의 중심좌표 입니다
             radius: meter, // 미터 단위의 원의 반지름입니다
-            strokeWeight: 5, // 선의 두께입니다
-            strokeColor: '#75B8FA', // 선의 색깔입니다
+            strokeWeight: 2, // 선의 두께입니다
+            strokeColor: '#1540bf', // 선의 색깔입니다
             strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-            strokeStyle: 'dashed', // 선의 스타일 입니다
-            fillColor: '#CFE7FF', // 채우기 색깔입니다
-            fillOpacity: 0.7  // 채우기 불투명도 입니다
+            strokeStyle: 'inline', // 선의 스타일 입니다
+            fillColor: '#000000', // 채우기 색깔입니다
+            fillOpacity: 0.3  // 채우기 불투명도 입니다
         });
 
         circles.push(circle);
@@ -770,3 +770,274 @@ function panTo(position) {
 function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
 }
+
+
+//map 컨트롤러
+// 지도타입 컨트롤의 지도 또는 스카이뷰 버튼을 클릭하면 호출되어 지도타입을 바꾸는 함수입니다
+function setMapType(maptype) {
+    var roadmapControl = document.getElementById('btnRoadmap');
+    var skyviewControl = document.getElementById('btnSkyview');
+    if (maptype === 'roadmap') {
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+        roadmapControl.className = 'selected_btn';
+        skyviewControl.className = 'btn';
+    } else {
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+        skyviewControl.className = 'selected_btn';
+        roadmapControl.className = 'btn';
+    }
+}
+
+//로드맵
+var overlayOn = false, // 지도 위에 로드뷰 오버레이가 추가된 상태를 가지고 있을 변수
+    container = document.getElementById('container')
+
+var mapCenter = new kakao.maps.LatLng(33.45042 , 126.57091), // 지도의 중심좌표
+    mapOption = {
+        center: mapCenter, // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+var rvWrapper = document.getElementById('rvWrapper')
+var rvContainer = document.getElementById('roadview')
+
+// 로드뷰 객체를 생성합니다
+var rv = new kakao.maps.Roadview(rvContainer);
+
+// 좌표로부터 로드뷰 파노라마 ID를 가져올 로드뷰 클라이언트 객체를 생성합니다
+var rvClient = new kakao.maps.RoadviewClient();
+
+// 로드뷰에 좌표가 바뀌었을 때 발생하는 이벤트를 등록합니다
+kakao.maps.event.addListener(rv, 'position_changed', function() {
+
+    // 현재 로드뷰의 위치 좌표를 얻어옵니다
+    var rvPosition = rv.getPosition();
+
+    // 지도의 중심을 현재 로드뷰의 위치로 설정합니다
+    map.setCenter(rvPosition);
+
+    // 지도 위에 로드뷰 도로 오버레이가 추가된 상태이면
+    if(overlayOn) {
+        // 마커의 위치를 현재 로드뷰의 위치로 설정합니다
+        marker2.setPosition(rvPosition);
+    }
+});
+
+// 마커 이미지를 생성합니다
+var markImage2 = new kakao.maps.MarkerImage(
+    'https://t1.daumcdn.net/localimg/localimages/07/2018/pc/roadview_minimap_wk_2018.png',
+    new kakao.maps.Size(26, 46),
+    {
+        // 스프라이트 이미지를 사용합니다.
+        // 스프라이트 이미지 전체의 크기를 지정하고
+        spriteSize: new kakao.maps.Size(1666, 168),
+        // 사용하고 싶은 영역의 좌상단 좌표를 입력합니다.
+        // background-position으로 지정하는 값이며 부호는 반대입니다.
+        spriteOrigin: new kakao.maps.Point(705, 114),
+        offset: new kakao.maps.Point(13, 46)
+    }
+);
+
+// 드래그가 가능한 마커를 생성합니다
+var marker2 = new kakao.maps.Marker({
+    image : markImage2,
+    position: mapCenter,
+    draggable: true
+});
+
+// 마커에 dragend 이벤트를 등록합니다
+kakao.maps.event.addListener(marker2, 'dragend', function(mouseEvent) {
+
+    // 현재 마커가 놓인 자리의 좌표입니다
+    var position = marker2.getPosition();
+
+    // 마커가 놓인 위치를 기준으로 로드뷰를 설정합니다
+    toggleRoadview(position);
+});
+
+//지도에 클릭 이벤트를 등록합니다
+kakao.maps.event.addListener(map, 'click', function(mouseEvent){
+
+    // 지도 위에 로드뷰 도로 오버레이가 추가된 상태가 아니면 클릭이벤트를 무시합니다
+    if(!overlayOn) {
+        return;
+    }
+
+    // 클릭한 위치의 좌표입니다
+    var position = mouseEvent.latLng;
+
+    // 마커를 클릭한 위치로 옮깁니다
+    marker2.setPosition(position);
+
+    // 클락한 위치를 기준으로 로드뷰를 설정합니다
+    toggleRoadview(position);
+});
+
+// 전달받은 좌표(position)에 가까운 로드뷰의 파노라마 ID를 추출하여
+// 로드뷰를 설정하는 함수입니다
+function toggleRoadview(position){
+    rvClient.getNearestPanoId(position, 50, function(panoId) {
+        // 파노라마 ID가 null 이면 로드뷰를 숨깁니다
+        console.log(`panoId ${panoId}`)
+        if (panoId === null) {
+            toggleMapWrapper(true, position);
+        } else {
+            toggleMapWrapper(false, position);
+
+            // panoId로 로드뷰를 설정합니다
+            rv.setPanoId(panoId, position);
+        }
+    });
+}
+
+// 지도를 감싸고 있는 div의 크기를 조정하는 함수입니다
+function toggleMapWrapper(active, position) {
+    console.log(`active ${active}`)
+    if (active) {
+        rvWrapper.style.display = 'none'
+
+        // 지도를 감싸고 있는 div의 너비가 100%가 되도록 class를 변경합니다
+        container.className = '';
+
+        // 지도의 크기가 변경되었기 때문에 relayout 함수를 호출합니다
+        map.relayout();
+
+        // 지도의 너비가 변경될 때 지도중심을 입력받은 위치(position)로 설정합니다
+        map.setCenter(position);
+    } else {
+
+        // 지도만 보여지고 있는 상태이면 지도의 너비가 50%가 되도록 class를 변경하여
+        // 로드뷰가 함께 표시되게 합니다
+        if (container.className.indexOf('view_roadview') === -1) {
+            container.className = 'view_roadview';
+            rvWrapper.style.display = 'block'
+
+            // 지도의 크기가 변경되었기 때문에 relayout 함수를 호출합니다
+            map.relayout();
+
+            // 지도의 너비가 변경될 때 지도중심을 입력받은 위치(position)로 설정합니다
+            map.setCenter(position);
+        }
+    }
+}
+
+// 지도 위의 로드뷰 도로 오버레이를 추가,제거하는 함수입니다
+function toggleOverlay(active) {
+    if (active) {
+        overlayOn = true;
+
+        // 지도 위에 로드뷰 도로 오버레이를 추가합니다
+        map.addOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+
+        // 지도 위에 마커를 표시합니다
+        marker2.setMap(map);
+
+        // 마커의 위치를 지도 중심으로 설정합니다
+        marker2.setPosition(map.getCenter());
+
+        // 로드뷰의 위치를 지도 중심으로 설정합니다
+        toggleRoadview(map.getCenter());
+    } else {
+        overlayOn = false;
+
+        // 지도 위의 로드뷰 도로 오버레이를 제거합니다
+        map.removeOverlayMapTypeId(kakao.maps.MapTypeId.ROADVIEW);
+
+        // 지도 위의 마커를 제거합니다
+        marker2.setMap(null);
+    }
+}
+
+// 지도 위의 로드뷰 버튼을 눌렀을 때 호출되는 함수입니다
+function setRoadviewRoad() {
+    var control = document.getElementById('roadviewControl');
+
+    // 버튼이 눌린 상태가 아니면
+    if (control.className.indexOf('active') === -1) {
+        control.className = 'active';
+
+        // 로드뷰 도로 오버레이가 보이게 합니다
+        toggleOverlay(true);
+    } else {
+        control.className = '';
+
+        // 로드뷰 도로 오버레이를 제거합니다
+        toggleOverlay(false);
+    }
+}
+
+// 로드뷰에서 X버튼을 눌렀을 때 로드뷰를 지도 뒤로 숨기는 함수입니다
+function closeRoadview() {
+    var position = marker2.getPosition();
+    toggleMapWrapper(true, position);
+}
+
+// 지도 타입 정보를 가지고 있을 객체입니다
+// map.addOverlayMapTypeId 함수로 추가된 지도 타입은
+// 가장 나중에 추가된 지도 타입이 가장 앞에 표시됩니다
+// 이 예제에서는 지도 타입을 추가할 때 지적편집도, 지형정보, 교통정보, 자전거도로 정보 순으로 추가하므로
+// 자전거 도로 정보가 가장 앞에 표시됩니다
+var mapTypes = {
+    terrain : kakao.maps.MapTypeId.TERRAIN,
+    traffic :  kakao.maps.MapTypeId.TRAFFIC,
+    bicycle : kakao.maps.MapTypeId.BICYCLE,
+    useDistrict : kakao.maps.MapTypeId.USE_DISTRICT
+};
+
+// 체크 박스를 선택하면 호출되는 함수입니다
+function setOverlayMapTypeId() {
+    var chkTerrain = document.getElementById('chkTerrain'),
+        chkTraffic = document.getElementById('chkTraffic'),
+        chkBicycle = document.getElementById('chkBicycle'),
+        chkUseDistrict = document.getElementById('chkUseDistrict');
+
+    // 지도 타입을 제거합니다
+    for (var type in mapTypes) {
+        map.removeOverlayMapTypeId(mapTypes[type]);
+    }
+
+    // 지적편집도정보 체크박스가 체크되어있으면 지도에 지적편집도정보 지도타입을 추가합니다
+    if (chkUseDistrict.checked) {
+        map.addOverlayMapTypeId(mapTypes.useDistrict);
+    }
+
+    // 지형정보 체크박스가 체크되어있으면 지도에 지형정보 지도타입을 추가합니다
+    if (chkTerrain.checked) {
+        map.addOverlayMapTypeId(mapTypes.terrain);
+    }
+
+    // 교통정보 체크박스가 체크되어있으면 지도에 교통정보 지도타입을 추가합니다
+    if (chkTraffic.checked) {
+        map.addOverlayMapTypeId(mapTypes.traffic);
+    }
+
+    // 자전거도로정보 체크박스가 체크되어있으면 지도에 자전거도로정보 지도타입을 추가합니다
+    if (chkBicycle.checked) {
+        map.addOverlayMapTypeId(mapTypes.bicycle);
+    }
+}
+
+const overlayviewControls = document.getElementById('overlayviewControls')
+const overlayviewControlsWin = document.querySelector('.overlayviewControls_win')
+const kakaoCheckboxs = document.querySelectorAll('input.kakao_menu')
+
+overlayviewControls.addEventListener('click',()=>{
+    let checkArr = []
+    overlayviewControls.classList.add('active')
+    overlayviewControlsWin.classList.toggle('active')
+    kakaoCheckboxs.forEach((el, idx)=>{
+        if(el.checked){
+            checkArr.push(el)
+        }else{
+            checkArr.filter((el)=> !el)
+        }
+    })
+
+    if(checkArr.length > 0){
+        overlayviewControls.classList.add('active')
+    }else{
+        if(overlayviewControlsWin.classList.contains('active')) return
+        overlayviewControls.classList.remove('active')
+    }
+    console.log(checkArr)
+ }
+)
